3a95626d9bbb5107a553d440de92eb28763b081f
Diogo Henrique Cruz Souza <100419674+DiogoSouza86@users.noreply.github.com>
2023-12-20 20:01:27 -0300

    aula 107

--- FlappyBird3D/Assets/SKIPTS/Principal.cs
+++ FlappyBird3D/Assets/SKIPTS/Principal.cs
@@ -13,11 +13,34 @@ public class Principal : MonoBehaviour
     void Start()
     {
         InvokeRepeating("CriaCerca", 1, 0.4f);
+        InvokeRepeating("CriaObstaculo", 1, 0.75f);
     }
 
     // Update is called once per frame
     void CriaCerca()
     {
         Instantiate(cerca);
+    }
+
+    void CriaObstaculo()
+    {
+        int sorteiaObjeto = Random.Range(1,5);
+        float posicaoXrandom = Random.Range(-2.78f, 2.28f);
+        float posicaoYrandom = Random.Range(0.98f, 3.25f);
+        float rotacaoYrandom = Random.Range(0.0f, 360.0f);
+
+        GameObject novoObjeto = new GameObject();
+
+        switch(sorteiaObjeto){
+        case 1 : novoObjeto = Instantiate(arbusto); posicaoYrandom = novoObjeto.transform.position.y; break;
+        case 2 : novoObjeto = Instantiate(pedra); posicaoYrandom = novoObjeto.transform.position.y; break;
+        case 3 : novoObjeto = Instantiate(nuvem); break;
+        case 4 : novoObjeto = Instantiate(canos); posicaoYrandom = Random.Range(-2.18f, 0.0f); posicaoXrandom = novoObjeto.transform.position.x; break;
+        default: break;
+        }
+
+        novoObjeto.transform.position = new Vector3(posicaoXrandom, posicaoYrandom,novoObjeto.transform.position.z);
+        novoObjeto.transform.rotation = Quaternion.Euler(novoObjeto.transform.rotation.x, rotacaoYrandom, novoObjeto.transform.rotation.y);
+
     }
 }
