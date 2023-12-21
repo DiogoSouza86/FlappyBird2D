a0178773a5fad8ebff5582516302eaa09aef836c
Diogo Henrique Cruz Souza <100419674+DiogoSouza86@users.noreply.github.com>
2023-12-21 15:44:24 -0300

    fisica e controle de personagem

--- FlappyBird3D/Assets/SKIPTS/Principal.cs
+++ FlappyBird3D/Assets/SKIPTS/Principal.cs
@@ -10,13 +10,31 @@ public class Principal : MonoBehaviour
     public GameObject arbusto;
     public GameObject canos;
 
-    void Start()
+    public GameObject jogadorFelpudo;
+
+    bool comecou;
+    bool acabou;
+
+    void Start(){
+        Physics.gravity = new Vector3(0.0f, -20.0f, 0.0f);
+    }
+    
+    void Update()
     {
-        InvokeRepeating("CriaCerca", 1, 0.4f);
-        InvokeRepeating("CriaObstaculo", 1, 0.75f);
+        if(Input.anyKeyDown){
+            if(!comecou){
+                comecou = true;
+                InvokeRepeating("CriaCerca", 1, 0.4f);
+                InvokeRepeating("CriaObstaculo", 1, 0.75f);
+
+                jogadorFelpudo.GetComponent<Rigidbody>().useGravity = true;
+                jogadorFelpudo.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            VoaFelpudo();
+        }
+        jogadorFelpudo.transform.rotation = Quaternion.Euler(jogadorFelpudo.GetComponent<Rigidbody>().velocity.y* -3,0,0); 
     }
 
-    // Update is called once per frame
     void CriaCerca()
     {
         Instantiate(cerca);
@@ -43,5 +61,10 @@ public class Principal : MonoBehaviour
         novoObjeto.transform.position = new Vector3(posicaoXrandom, posicaoYrandom, novoObjeto.transform.position.z);
         novoObjeto.transform.rotation = Quaternion.Euler(novoObjeto.transform.rotation.x, rotacaoYrandom, novoObjeto.transform.rotation.y);
 
+    }
+
+    void VoaFelpudo(){
+        jogadorFelpudo.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        jogadorFelpudo.GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 10.0f,0.0f);
     }
 }
